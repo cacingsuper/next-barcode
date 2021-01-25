eaae98492d35623525b7d63d1d53b25ed0934ed7
Lamtoro <danny_lamttoro@yahoo.com>
2021-01-25 12:03:27 +0700

    qr

--- components/Button/Download.tsx
+++ components/Button/Download.tsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 function Download({onClick}) {
     return (
-        <div className='bg-indigo-900 mt-5 w-full py-2 rounded'>
-          <button onClick={onClick} className="w-full bg-transparent" >Download</button>  
+        <div className='mt-5 w-full py-2 rounded'>
+          <button onClick={onClick} className="w-full py-2 px-4 bg-green-500 text-white font-semibold rounded-lg shadow-md hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-green-400 focus:ring-opacity-75" >Download</button>  
         </div>
     )
 }
